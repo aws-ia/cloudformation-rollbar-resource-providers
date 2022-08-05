52cba9217164a7741f659291d8dabfaaddc768eb
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2022-08-05 14:49:24 +0200

    Add common rollbar code

---
+++ Rollbar-Common/src/rollbar-client.ts
@@ -0,0 +1,37 @@
+import axios, {AxiosResponse} from "axios";
+
+export type ApiError = {
+    message?: string
+    err: number
+}
+
+export type PaginatedResponseType = {
+    offset: number
+    limit: number
+    more: boolean
+    total: number
+}
+
+export class RollbarClient {
+    private readonly apiToken: string;
+    private readonly userAgent: string;
+
+    constructor(apiToken: string, userAgent?: string) {
+        this.apiToken = apiToken;
+        this.userAgent = userAgent;
+    }
+
+    public async doRequest<ResponseType>(method: 'get' | 'put' | 'post' | 'patch' | 'delete', path: string, params: any = {}, body?: {}, headers?: {[key: string]: string}): Promise<AxiosResponse<ResponseType>> {
+        return await axios.request<ResponseType>({
+            url: `https://api.rollbar.com${path}`,
+            params: params,
+            method: method,
+            data: body,
+            headers: {
+                ...headers,
+                'X-Rollbar-Access-Token': this.apiToken,
+                'User-Agent': this.userAgent || "AWS CloudFormation (+https://aws.amazon.com/cloudformation/) CloudFormation custom resource"
+            }
+        });
+    }
+}
