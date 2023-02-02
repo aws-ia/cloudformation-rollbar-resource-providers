3c951506ecb5339dea11bff0fe5b4e43f2ee6265
Rajesh Duraisamy <rajdnp@amazon.com>
2023-02-02 23:05:21 +0000

    Adding Rollbar-notification-rule

---
+++ Rollbar-Notifications-Rule/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Set up any prerequisites needed for cfn test
+#
+mkdir -p inputs
+cat example_inputs/inputs_1_create.json | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_1_create.json
+cat example_inputs/inputs_1_invalid.json | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_1_invalid.json
+cat example_inputs/inputs_1_update.json  | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_1_update.json
+cat example_inputs/inputs_2_create.json | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_2_create.json
+cat example_inputs/inputs_2_update.json  | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_2_update.json
+cat example_inputs/inputs_3_create.json | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_3_create.json
+cat example_inputs/inputs_3_update.json  | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_3_update.json
+cat example_inputs/inputs_4_create.json | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_4_create.json
+cat example_inputs/inputs_4_update.json  | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > inputs/inputs_4_update.json
+cat test/integ-template.yml | sed "s/<ROLLBAR_PROJECT_TOKEN>/${ROLLBAR_PROJECT_TOKEN}/g" > test/integ.yml
+
