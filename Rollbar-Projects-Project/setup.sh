5692cc3cb0f6a7579aeecbe23244037349fb940c
Rajesh Duraisamy <rajdnp@amazon.com>
2023-02-02 19:36:26 +0000

    Removing unused files and moving pagerduty key to parameter store

--- Rollbar-Projects-Project/setup.sh
+++ Rollbar-Projects-Project/setup.sh
@@ -3,7 +3,7 @@
 # Set up any prerequisites needed for cfn test
 #
 mkdir -p inputs
-cat example_inputs/inputs_1_create.json | sed "s/GROUP_ID/${GROUP_ID}/g" > inputs/inputs_1_create.json
-cat example_inputs/inputs_1_update.json  | sed "s/GROUP_ID/${GROUP_ID}/g" > inputs/inputs_1_update.json
-cat test/integ-template.yml | sed "s/GROUP_ID/${GROUP_ID}/g" > test/integ.yml
+cat example_inputs/inputs_1_create.json | sed "s/PAGER_DUTY_SERVICEKEY/${PAGER_DUTY_SERVICEKEY}/g" > inputs/inputs_1_create.json
+cat example_inputs/inputs_1_update.json  | sed "s/PAGER_DUTY_SERVICEKEY/${PAGER_DUTY_SERVICEKEY}/g" > inputs/inputs_1_update.json
+cat test/integ-template.yml | sed "s/PAGER_DUTY_SERVICEKEY/${PAGER_DUTY_SERVICEKEY}/g" > test/integ.yml
 
