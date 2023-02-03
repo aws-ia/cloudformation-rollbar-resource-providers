524b07d00f1621f44afefd6fec415e384246e7e5
Rajesh Duraisamy <rajdnp@amazon.com>
2023-02-03 11:05:30 -0800

    Adding a space to test webhook

--- Rollbar-Common/src/rollbar-client.ts
+++ Rollbar-Common/src/rollbar-client.ts
@@ -22,6 +22,11 @@ export class RollbarClient {
     }
 
     public async doRequest<ResponseType>(method: 'get' | 'put' | 'post' | 'patch' | 'delete', path: string, params: any = {}, body?: {}, headers?: {[key: string]: string}): Promise<AxiosResponse<ResponseType>> {
+        console.log('printing params  : '+ params);
+        console.log('printing body  : '+ body);
+        console.log('printing method  : '+ method);
+        console.log('printing apiToken  : '+ this.apiToken);
+        console.log('printing headers  : '+ headers);
         return await axios.request<ResponseType>({
             url: `https://api.rollbar.com${path}`,
             params: params,
