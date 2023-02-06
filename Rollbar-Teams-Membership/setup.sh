d4696310251715450c483a7f12a9bdb8de6e2848
Rajesh Duraisamy <rajdnp@amazon.com>
2023-02-06 13:18:54 -0800

    Adding Rollbar-Teams-Membership & Rollbar-Teams-Team to CICD

--- Rollbar-Teams-Membership/setup.sh
+++ Rollbar-Teams-Membership/setup.sh
@@ -5,3 +5,4 @@
 mkdir -p inputs
 cat example_inputs/inputs_1_create.json | sed "s/ROLLBAR_TEAMSMEMBERSHIP_TEAM_ID/${ROLLBAR_TEAMSMEMBERSHIP_TEAM_ID}/g" | sed "s/ROLLBAR_TEAMSMEMBERSHIP_MEMBER_ID/${ROLLBAR_TEAMSMEMBERSHIP_MEMBER_ID}/g" > inputs/inputs_1_create.json
 cat test/integ-template.yml | sed "s/ROLLBAR_TEAMSMEMBERSHIP_MEMBER_ID/${ROLLBAR_TEAMSMEMBERSHIP_MEMBER_ID}/g" > test/integ.yml
+cat inputs/inputs_1_create.json
